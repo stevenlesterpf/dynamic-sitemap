56ddfb64807153e4b39ab3c4bcc2c8a75dc30dbe
stevenlesterpf <steven.lester@peoplefinders.com>
2026-05-15 20:56:51 +0800

    Initial commit

---
+++ DataAggregator/Program.cs
@@ -0,0 +1,7 @@
+using DataAggregator;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ SitemapManager/Program.cs
@@ -0,0 +1,7 @@
+using SitemapManager;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
